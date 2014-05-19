fcac95ca8b7a664ff8bf497e9233869d4d9d0d55
Adrian.Moore <stewsters@gmail.com>
2014-05-19 16:37:43 -0500

    Splitting ai

--- core/src/main/groovy/com/stewsters/zomgrl/entity/components/ai/AdvancedStats.groovy
+++ core/src/main/groovy/com/stewsters/zomgrl/entity/components/ai/AdvancedStats.groovy
@@ -7,6 +7,17 @@ import com.stewsters.zomgrl.entity.components.item.Slot
 
 public class AdvancedStats extends BaseAi implements Ai {
 
+    private float morale
+    private float chargeProbability
+    private float retreatProbability
+
+    public AdvancedStats(Map params) {
+        morale = params.morale ?: 0.5f
+        chargeProbability = params.chargeProbability ?: 0.5f
+        retreatProbability = params.retreatProbability ?: 0.5f
+    }
+
+    //http://dillingers.com/blog/2014/05/10/roguelike-ai/
     public void takeTurn() {
 
         //nearest opponent
@@ -14,6 +25,38 @@ public class AdvancedStats extends BaseAi implements Ai {
 
         if (enemy) {
             //if we have a gun, and they are getting too close, shoot them
+//            if(owner.fighter && ((owner.fighter.hp/owner.fighter.maxHP ) < morale )){
+//                //flee
+//                if(!owner.moveAway(enemy.x,enemy.y)){
+//                    owner.moveTowardsAndAttack(enemy.x,enemy.y)
+//                }
+//            }
+//            else if too-far-from-player
+//                AND can-attack-player
+//                AND can-move-toward-player
+//                if  random < charge-probability
+//                    move-toward-player
+//                else attack-player
+//            else if too-close-to-character
+//                AND can-attack-player
+//                AND can-move-away-from-player
+//                if random < retreat-probability
+//                    move-away-from-player
+//                else attack-player
+//            else if can-attack-player
+//                attack-player
+//            else if too-far-from-player
+//                AND can-move-toward-player
+//                move-toward-player
+//            else if too-close-to-player
+//                AND can-move-away-from-player
+//                move-away-from-player
+//            else if (MatUtils.boolean) {
+//                owner.randomMovement();
+//            }
+
+
+
 
             if (owner.distanceTo(enemy) < 2) {
                 owner.moveTowardsAndAttack(enemy.x, enemy.y)
